b54210b93cef956791ceff076867659a43bdd368
clothing-site <zhomart05025@gmail.com>
2025-10-23 23:09:01 +0500

    Update GoodWardrobe.js

--- js/goods/GoodWardrobe.js
+++ js/goods/GoodWardrobe.js
@@ -1,83 +1,82 @@
-
 const wardrobes = [
 	{
 		"id": "2",
-		"title": "Шкаф-купе 'Classic'",
+		"title": "«Classic» купе шкафы",
 		"art": 9958,
 		"price": 45600,
-		"text": "Классическая система хранения с доп.полками",
+		"text": "Қосымша сөрелері бар классикалық сақтау жүйесі",
 		"imgSrc": "ward(1).png"
 	},
 	{
 		"id": "5",
-		"title": "Шкаф-купе 'Uni'",
+		"title": "«Uni» купе шкафы",
 		"art": 2854,
 		"price": 36500,
-		"text": "Шкаф купе с опциональными зеркальными дверцами",
+		"text": "Айна есігі бар опциялы купе шкаф",
 		"imgSrc": "ward(5).png"
 	},
 	{
 		"id": "1",
-		"title": "Система хранения 'All'",
+		"title": "«All» сақтау жүйесі",
 		"art": 818952,
 		"price": 73000,
-		"text": "Система хранения во всю стену (только под заказ)",
+		"text": "Бүкіл қабырға бойымен сақтау жүйесі (тек тапсырыспен)",
 		"imgSrc": "ward(4).png"
 	},
 	{
 		"id": "4",
-		"title": "Гардероб 'Softy'",
+		"title": "«Softy» гардеробы",
 		"art": 82784,
 		"price": 23500,
-		"text": "Три широких отсека для хранения со штангами",
+		"text": "Киім ілетін штангасы бар үш кең бөлім",
 		"imgSrc": "ward(6).png"
 	},
 	{
 		"id": "3",
-		"title": "Гардероб 'Extra'",
+		"title": "«Extra» гардеробы",
 		"art": 8828846,
 		"price": 61000,
-		"text": "Вместительный гардероб с дополнительными полками",
+		"text": "Қосымша сөрелері бар кең гардероб",
 		"imgSrc": "ward(9).png"
 	},
 	{
 		"id": "8",
-		"title": "Шкаф для обуви",
+		"title": "Аяқ киімге арналған шкаф",
 		"art": 8248,
 		"price": 29000,
-		"text": "Распашной шкаф для обуви а аксесуаров",
+		"text": "Аяқ киім мен аксессуарларға арналған есігі ашылатын шкаф",
 		"imgSrc": "ward(3).png"
 	},
 	{
 		"id": "0",
-		"title": "Платьяной шкаф",
+		"title": "Көйлек шкабы",
 		"art": 7889568,
 		"price": 33000,
-		"text": "Платьяной шкаф с полочками. Расположение полочек и штанги может быть зеркальным",
+		"text": "Сөрелері бар көйлек шкабы. Сөре мен штанга орналасуы айна тәрізді болуы мүмкін",
 		"imgSrc": "ward(8).png"
 	},
 	{
 		"id": "7",
-		"title": "Шкаф для хранения",
+		"title": "Сақтау шкафы",
 		"art": 2693988,
 		"price": 22000,
-		"text": "Деревянный шкаф с дверками. Верхняя часть также может быть закрыта дверцами",
+		"text": "Ағаш шкаф есіктерімен. Жоғарғы бөлік те есіктермен жабылуы мүмкін",
 		"imgSrc": "ward(10).png"
 	},
 	{
 		"id": "6",
-		"title": "Трюмо 'Прованс'",
+		"title": "«Прованс» трюмосы",
 		"art": 952632,
 		"price": 39000,
-		"text": "Платьяной шкаф на ножках. Укомплектован штангой для одежды и полками",
+		"text": "Аяқта тұратын көйлек шкафы. Киімге арналған штанга және сөрелермен жабдықталған",
 		"imgSrc": "ward(7).png"
 	},
 	{
 		"id": "9",
-		"title": "Шкаф Магнум с зеркалом",
+		"title": "«Магнум» айнасы бар шкаф",
 		"art": 2763348,
 		"price": 54000,
-		"text": "Плетёное кресло со сьёмными чехлами",
+		"text": "Алынбалы қаптамасы бар өрілген кресло",
 		"imgSrc": "ward(2).png"
 	}
 ]
@@ -91,7 +90,7 @@ wardrobes.forEach ((ward) => {
     `
 	<div class="products-item" data-id="${ward.id}">
 		<div class="products-item-img"> 
-			<img src="./img/items/wardrobe/${ward.imgSrc}" alt="Упс..." class="productImg">
+			<img src="./img/items/wardrobe/${ward.imgSrc}" alt="Кешіріңіз..." class="productImg">
 		</div>
 
 		<div class="products-item-title ">${ward.title}</div>
@@ -106,17 +105,16 @@ wardrobes.forEach ((ward) => {
 			</div>
 
 			<div class="price">
-				<div class="price__currency">${ward.price} ₽</div>
+				<div class="price__currency">${ward.price} ₸</div>
 			</div>
 		</div>
 
 		<div class="products-action">
-			<button data-cart class="button glow-on-hover product-button"> В корзину</button>
+			<button data-cart class="button glow-on-hover product-button"> Себетке</button>
 		</div>
-		<div class="price__weight">Добавлено: ${dataUpdate}</div>
+		<div class="price__weight">Қосылған күні: ${dataUpdate}</div>
 	</div>
 
     `
 })
 wardrobeItem.insertAdjacentHTML('beforeend', wardItem);
-
